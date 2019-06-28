30880427b0505ff51eb6bbeb661595b2583b3cc8
NMojica <nmojica@signalsecure.com>
2019-06-28 12:31:24 -0600

    (cargo-release) generate docs

---
+++ implementors/core/marker/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["fluentbit"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"struct\" href=\"fluentbit/struct.PluginInfo.html\" title=\"struct fluentbit::PluginInfo\">PluginInfo</a>",synthetic:true,types:["fluentbit::fluent::PluginInfo"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Sync.html\" title=\"trait core::marker::Sync\">Sync</a> for <a class=\"enum\" href=\"fluentbit/enum.FLBError.html\" title=\"enum fluentbit::FLBError\">FLBError</a>",synthetic:true,types:["fluentbit::fluent::FLBError"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["fluentbit"] = {"name":"","dirs":[],"files":["fluent.rs","lib.rs"]};
+createSourceSidebar();
